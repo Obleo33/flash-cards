b51135ec49f9535617234b1170b5cb02cb5f9173
Franklin Crosby <obleo33@gmail.com>
2017-02-08 15:05:08 -0700

    Testing for number correct and card advancement

--- scripts/round.js
+++ scripts/round.js
@@ -4,15 +4,24 @@ class Round{
   constructor(deck){
     this.deck = deck;
     this.guesses = [];
+    this.numberCorrect = 0;
   }
 
   currentCard(){
-    return this.deck.cards[0];
+    return this.deck.cards[this.numberCorrect];
   }
 
-  recordGuess(response){
-    console.log(currentCard())
-    let guess = new Guess({guess:response,card});
+  recordGuess(userGuess){
+    let card = this.currentCard();
+    let guess = new Guess({response: userGuess,card});
+    this.guesses.push(guess);
+    if (guess.correct === true){
+      this.numberCorrect ++;
+    }
+  }
+
+  count(){
+    return this.guesses.length;
   }
 }
 
--- tests/round-test.js
+++ tests/round-test.js
@@ -31,7 +31,7 @@ describe('Round constructor', () => {
   })
 
   it('should have a currentCard function', () => {
-    let card1 = new Card({question: "What is the capital of Alaska?", answer: "Juneau"})
+    let card1 = new Card({question: 'What is the capital of Alaska?', answer: 'Juneau'})
     let deck  = new Deck({});
     deck.addCard(card1);
     let round = new Round(deck);
@@ -40,8 +40,8 @@ describe('Round constructor', () => {
   })
 
   it('should return the first card in the deck when you call currentCard', () => {
-    let card1 = new Card({question: "What is the capital of Alaska?", answer: "Juneau"})
-    let card2 = new Card({question: "Approximately how many miles are in one astronomical unit?", answer: "93,000,000"})
+    let card1 = new Card({question: 'What is the capital of Alaska?', answer: 'Juneau'})
+    let card2 = new Card({question: 'Approximately how many miles are in one astronomical unit?', answer: '93,000,000'})
     let deck  = new Deck({});
 
     deck.addCard(card1);
@@ -53,28 +53,108 @@ describe('Round constructor', () => {
   })
 
   it('should have a recordGuess function', () => {
-    let card1 = new Card({question: "What is the capital of Alaska?", answer: "Juneau"})
-    let card2 = new Card({question: "Approximately how many miles are in one astronomical unit?", answer: "93,000,000"})
+    let card1 = new Card({question: 'What is the capital of Alaska?', answer: 'Juneau'})
+    let card2 = new Card({question: 'Approximately how many miles are in one astronomical unit?', answer: '93,000,000'})
     let deck  = new Deck({});
 
     deck.addCard(card1);
     deck.addCard(card2);
-
     let round = new Round(deck);
 
     expect(round.recordGuess('')).to.be.function;
   })
 
-  xit('should create a new guess card and push it to the guesses array when you run recordGuess', () => {
-    let round = new Round();
-    recordGuess('guess');
+  it('should create a new guess card and push it to the guesses array when you run recordGuess', () => {
+    let card1 = new Card({question: 'What is the capital of Alaska?', answer: 'Juneau'})
+    let card2 = new Card({question: 'Approximately how many miles are in one astronomical unit?', answer: '93,000,000'})
+    let deck  = new Deck({});
 
-    expect(this.guesses.length).to.equal(1);
+    deck.addCard(card1);
+    deck.addCard(card2);
+    let round = new Round(deck);
+
+    round.recordGuess("Juneau")
+    expect(round.guesses.length).to.equal(1);
   })
 
 
-  xit('should step through the deck as you answer correctly', () => {
+  it('should have a count function that counts how many guesses you have made', () => {
+    let card1 = new Card({question: 'What is the capital of Alaska?', answer: 'Juneau'})
+    let card2 = new Card({question: 'Approximately how many miles are in one astronomical unit?', answer: '93,000,000'})
+    let deck  = new Deck({});
 
+    deck.addCard(card1);
+    deck.addCard(card2);
+    let round = new Round(deck);
+
+    round.recordGuess("Juneau")
+
+    expect(round.guesses.length).to.equal(1);
+    expect(round.count()).to.be.function;
+    expect(round.count()).to.equal(1);
+
+    round.recordGuess("Juneau")
+
+    expect(round.count()).to.equal(2);
+  })
+
+  it('should have a update the numberCorrect if your guess was correct', () => {
+    let card1 = new Card({question: 'What is the capital of Alaska?', answer: 'Juneau'})
+    let card2 = new Card({question: 'Approximately how many miles are in one astronomical unit?', answer: '93,000,000'})
+    let deck  = new Deck({});
+
+    deck.addCard(card1);
+    deck.addCard(card2);
+    let round = new Round(deck);
+
+    expect(round.numberCorrect).to.equal(0);
+
+    round.recordGuess("Juneau");
+
+    expect(round.numberCorrect).to.equal(1);
+  })
+
+  it('should step through the deck as you answer correctly', () => {
+    let card1 = new Card({question: 'What is the capital of Alaska?', answer: 'Juneau'})
+    let card2 = new Card({question: 'Approximately how many miles are in one astronomical unit?', answer: '93,000,000'})
+    let deck  = new Deck({});
+
+    deck.addCard(card1);
+    deck.addCard(card2);
+    let round = new Round(deck);
+
+    expect(round.numberCorrect).to.equal(0);
+
+    round.recordGuess("Juneau");
+
+    expect(round.numberCorrect).to.equal(1);
+
+    expect(round.currentCard()).to.equal(card2);
+  })
+
+  it('should allow you to answer multiple times till you get the correct answer', () => {
+    let card1 = new Card({question: 'What is the capital of Alaska?', answer: 'Juneau'})
+    let card2 = new Card({question: 'Approximately how many miles are in one astronomical unit?', answer: '93,000,000'})
+    let deck  = new Deck({});
+
+    deck.addCard(card1);
+    deck.addCard(card2);
+    let round = new Round(deck);
+
+    round.recordGuess("hey");
+    expect(round.currentCard()).to.equal(card1);
+
+    round.recordGuess("you");
+    round.recordGuess("it's me");
+
+    expect(round.guesses.length).to.equal(3);
+
+    round.recordGuess("Juneau");
+
+    expect(round.guesses.length).to.equal(4);
+    expect(round.numberCorrect).to.equal(1);
+
+    expect(round.currentCard()).to.equal(card2);
   })
 
 });
